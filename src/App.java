1d131bd51c547fa3c7bf6e8495551b041e79cc9e
Windows <lainey.landreth@gmail.com>
2021-03-22 13:57:57 -0500

    first commit being made

---
+++ src/App.java
@@ -0,0 +1,82 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//Intro to Java: Week 1 Coding Assignment
+		//3. Real Life Examples:
+
+//a.		
+		double itemPrice1 = 13.99;
+		double itemPrice2 = 8.57;
+		
+		
+//b.
+		
+		double amtMoneyInAccount1 = 6.38;
+		double amtMoneyInAccount2 = 9.45;
+		
+//c.
+		
+		double numberOfFriends1 = 65;
+	    double numberOfFriends2 = 48;
+	    
+//d.
+	    
+	    double ageInYears1 = 9;
+	    double ageInYears2 = 89;
+	    
+//e.
+	    
+	    String firstName1 = "Jamie";
+	    String firstName2 = "Colin";
+	    
+//f. 
+	    String lastName1 = "Smithens";
+	    String lastName2 = "Roger";
+	    
+//g.
+	    char middleInt1 = 'K';
+	    char middleInt2 = 'J';
+	    
+//4 + 5. Operations 
+	    
+//a. 
+	   double newAmountMoney1 = amtMoneyInAccount1 - itemPrice1;
+	   double newAmountMoney2 = amtMoneyInAccount2 - itemPrice2;
+	  
+	   System.out.println("After buying my mom's birthday present, I had $" + newAmountMoney1 + " in my bank account");
+	   System.out.println("After buying my dad's birthday present, I had $" + newAmountMoney2 + " in my bank account");
+//b.
+	   double amtFriendsPerYear1 = numberOfFriends1 / ageInYears1; 
+	   double amtFriendsPerYear2 = numberOfFriends2 / ageInYears2;
+	  
+	   System.out.println("At the time of my death, I had made " + amtFriendsPerYear1 + " friends per year.");
+	   System.out.println("At the time of my Cousin's death, he had made " + amtFriendsPerYear2 + " friends per year.");
+	  
+//c.
+	   
+	   String fullName1 = firstName1 + " " + middleInt1 + " "+ lastName1; 
+	   String fullName2 = firstName2 + " " + middleInt2 + " "+ lastName2;
+	   
+	   System.out.println("My full, God-given name, is " + fullName1);
+	   System.out.println("My cousin's full name, which he changed in highschool, is " + fullName2);
+	   
+	  
+	   
+	   
+	   
+	   
+	   
+	   
+	  
+	    
+	    		
+	    		
+	    		
+		
+		
+	}
+
+}
